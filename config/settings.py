2b610ecb55de49282b43d65cec6f8a98fde5a09b
Yusuf <109858565+yusufunity@users.noreply.github.com>
2022-08-19 18:59:17 +0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jc%!#+@r^*gsfp8yh)zxi-%w4+l24zw*k%^%+$oqsrr8qdj%$j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['178.128.248.215']
 
 
 # Application definition
